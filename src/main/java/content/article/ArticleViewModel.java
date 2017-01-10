553152cbf6c8a8cf256ad473729f11074e43b371
Ryan-Anderson <ryan@perfectsensedigital.com>
2017-01-10 15:06:58 -0500

    Import ArticleView package

--- src/main/java/content/article/ArticleViewModel.java
+++ src/main/java/content/article/ArticleViewModel.java
@@ -1,16 +1,17 @@
 package content.article;
 
 import com.psddev.cms.view.ViewModel;
+import styleguide.content.article.ArticleView;
 
-public class ArticleViewModel extends ViewModel<Article> implements styleguide.content.article.ArticleView {
-
-    @Override
-    public String getHeadline() {
-        return model.getHeadline();
-    }
+public class ArticleViewModel extends ViewModel<Article> implements ArticleView {
 
     @Override
     public String getBody() {
         return model.getBody();
+    }
+
+    @Override
+    public String getHeadline() {
+        return model.getHeadline();
     }
 }
