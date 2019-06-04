d84ece5c96fe81bd9d3f669bb699e794a2e65649
Dan Slaughter <dan@perfectsensedigital.com>
2019-06-04 09:10:10 -0400

    Bumps BSP version to 4.1.5

--- hello-world/src/main/java/brightspot/tutorial/article/ArticleViewModel.java
+++ hello-world/src/main/java/brightspot/tutorial/article/ArticleViewModel.java
@@ -5,6 +5,7 @@ import brightspot.tutorial.image.Image;
 import com.psddev.cms.rte.RichTextViewBuilder;
 import com.psddev.cms.view.ViewInterface;
 import com.psddev.cms.view.ViewModel;
+import com.psddev.dari.db.Database;
 import com.psddev.dari.util.StorageItem;
 import com.psddev.handlebars.HandlebarsTemplate;
 
@@ -30,7 +31,7 @@ public class ArticleViewModel extends ViewModel<Article> {
     public CharSequence getBody() {
         String body = model.getBody();
         if (body != null) {
-            return RichTextViewBuilder.buildHtml(body, (rte) -> null);
+            return RichTextViewBuilder.buildHtml(Database.Static.getDefault(), body, (rte) -> null);
         }
         return null;
     }
