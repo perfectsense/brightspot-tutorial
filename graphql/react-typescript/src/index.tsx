09273de72dfe5f0fcc7d1422737e77554109c473
Dan Slaughter <dan@perfectsensedigital.com>
2021-02-11 10:16:56 -0500

    Adds example react/typescript app based on this tutorial: https://codeburst.io/tooling-your-typescript-create-react-app-to-work-with-graphql-d845e035d44e

---
+++ graphql/react-typescript/src/index.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import {
+  ApolloProvider,
+  HttpLink,
+  ApolloClient,
+  InMemoryCache,
+} from "@apollo/client";
+
+const graphQLEndpoint = new HttpLink({
+  uri: "",
+});
+
+const cache = new InMemoryCache();
+
+const client = new ApolloClient({
+  link: graphQLEndpoint,
+  cache,
+  credentials: "include",
+  resolvers: {},
+});
+
+ReactDOM.render(
+  <React.StrictMode>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
