2333abc12926db89940b4121761ccee69471a7cf
Joseph Torralba <>
2021-03-28 20:30:36 -0700

    add image to content type

--- init/src/main/java/article/Article.java
+++ init/src/main/java/article/Article.java
@@ -2,6 +2,7 @@ package article;
 
 import com.psddev.cms.db.Content;
 import com.psddev.cms.db.ToolUi;
+import com.psddev.dari.util.StorageItem;
 
 public class Article extends Content {
 
@@ -11,11 +12,17 @@ public class Article extends Content {
     @ToolUi.RichText
     private String body;
 
+    private StorageItem image;
+
     public String getHeadline() {
         return headline;
     }
 
     public String getBody() {
         return body;
+    }
+
+    public StorageItem getImage() {
+        return image;
     }
 }
