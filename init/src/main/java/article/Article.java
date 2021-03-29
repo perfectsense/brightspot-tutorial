937f111c8ae64a9c74d46eb88d806517b8c77903
Joseph Torralba <>
2021-03-28 21:51:12 -0700

    add reference content (Author)

--- init/src/main/java/article/Article.java
+++ init/src/main/java/article/Article.java
@@ -3,15 +3,19 @@ package article;
 import com.psddev.cms.db.Content;
 import com.psddev.cms.db.ToolUi;
 import com.psddev.dari.util.StorageItem;
+import com.psddev.dari.util.StringUtils;
+import com.psddev.cms.db.Directory;
+import com.psddev.cms.db.Site;
+import author.Author;
 
-public class Article extends Content {
+public class Article extends Content  implements Directory.Item  {
 
     @Required
     private String headline;
 
     @ToolUi.RichText
     private String body;
-
+    private Author author;
     private StorageItem image;
 
     public String getHeadline() {
@@ -25,4 +29,10 @@ public class Article extends Content {
     public StorageItem getImage() {
         return image;
     }
+
+    @Override
+    public String createPermalink(Site site) {
+        return StringUtils.toNormalized(getHeadline());
+    }
+
 }
---
+++ init/src/main/java/author/Author.java
@@ -0,0 +1,11 @@
+package author;
+
+import com.psddev.cms.db.Content;
+
+public class Author extends Content  {
+
+    private String lastName;
+
+    private String firstName;
+
+}
