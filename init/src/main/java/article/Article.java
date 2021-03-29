e7f29ccc40ee06abe50f76b507cab63367dfffc9
Joseph Torralba <>
2021-03-28 20:27:23 -0700

    add rich-text editor to body

--- init/src/main/java/article/Article.java
+++ init/src/main/java/article/Article.java
@@ -1,11 +1,14 @@
 package article;
 
 import com.psddev.cms.db.Content;
+import com.psddev.cms.db.ToolUi;
 
 public class Article extends Content {
 
     @Required
     private String headline;
+
+    @ToolUi.RichText
     private String body;
 
     public String getHeadline() {
