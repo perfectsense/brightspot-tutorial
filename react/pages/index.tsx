7f320a6fd6c01c36249c74e7268423b882356899
Tom Hoppe <trhoppe@gmail.com>
2022-01-19 15:09:02 -0700

    Adding query to pull Raw HTML from my local docker

--- react/pages/index.tsx
+++ react/pages/index.tsx
@@ -1,7 +1,30 @@
 import React, { Fragment } from 'react'
+import { useQuery, gql } from '@apollo/client'
 import Head from 'next/head'
 
+const QUERY = gql`
+  query Home {
+    Text(id: "0000017e-744f-d0cd-a5ff-f47ff5100000") {
+      name
+      text
+      statusCode
+      _id
+    }
+  }
+`
+
 const Home = () => {
+  const { data, loading, error } = useQuery(QUERY)
+
+  if (loading) {
+    return <h2>Loading...</h2>
+  }
+
+  if (error) {
+    console.error(error)
+    return null
+  }
+
   return (
     <Fragment>
       <Head>
@@ -9,6 +32,7 @@ const Home = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <h1>Hello Brightspot!</h1>
+      {data.Text.text}
     </Fragment>
   )
 }
