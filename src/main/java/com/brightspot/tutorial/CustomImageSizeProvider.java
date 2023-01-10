e9302367546cd730c2ca0e4f891ba58d7be37984
Mandi Haase <haase1020@gmail.com>
2023-01-10 11:48:54 -0500

    update image sizes

--- src/main/java/com/brightspot/tutorial/CustomImageSizeProvider.java
+++ src/main/java/com/brightspot/tutorial/CustomImageSizeProvider.java
@@ -12,45 +12,34 @@ import com.psddev.cms.image.ImageSizeProvider;
 
 public class CustomImageSizeProvider implements ImageSizeProvider {
 
-    public static final ImageSize TALL = ImageSize.builder()
-            .displayName("3:2 Ratio Large")
-            .internalName("3-2-large")
-            .group("3:2 Ratio")
-            .width(1080)
-            .height(720)
-            .strip(false)
-            .srcsetDescriptors(new ArrayList<String>() {{add("1x"); add("2x");}})
-            .alternateFormats(new ArrayList<String>() {{add("webp");}})
-            .build();
-
     public static final ImageSize PORTRAIT = ImageSize.builder()
-            .displayName("16:9 Ratio")
-            .internalName("16-9")
-            .group("16:9 Ratio")
-            .width(1920)
-            .height(1080)
-            .srcsetDescriptors(new ArrayList<String>() {{add("1x"); add("2x");}})
+            .displayName("3:2 Portrait")
+            .internalName("3-2-portrait")
+            .group("3:2 Ratio")
+            .width(600)
+            .height(400)
+            .strip(false)
+            .srcsetDescriptors(new ArrayList<String>() {{add("100w");}{add("200w");}{add("300w");} {add("400w");} {add("500w");} {add("600w");}})
             .alternateFormats(new ArrayList<String>() {{add("webp");}})
             .build();
 
-    public static final ImageSize SOCIAL_MEDIA = ImageSize.builder()
-            .displayName("9:16 Ratio")
-            .internalName("9-16")
-            .group("9:16 Ratio")
-            .width(1080)
-            .height(1920)
-            .srcsetDescriptors(new ArrayList<String>() {{add("1x"); add("2x");}})
+    public static final ImageSize TALL = ImageSize.builder()
+            .displayName("2:3 Tall")
+            .internalName("2-3-tall")
+            .group("2:3 Ratio")
+            .width(400)
+            .height(600)
+            .srcsetDescriptors(new ArrayList<String>() {{add("100w");}{add("200w");}{add("300w");} {add("400w");}})
             .alternateFormats(new ArrayList<String>() {{add("webp");}})
-            .quality(90) // default is 90
             .build();
 
     public static final ImageSize SQUARE = ImageSize.builder()
-            .displayName("1:1 Ratio Large")
-            .internalName("1-1-large")
+            .displayName("1:1 Square")
+            .internalName("1-1-square")
             .group("1:1 Ratio")
-            .width(1080)
-            .height(1080)
-            .srcsetDescriptors(new ArrayList<String>() {{add("300w");} {add("400w");} {add("500w");} {add("600w");} {add("700w");} {add("800w");} {add("900w");} {add("1000w");}})
+            .width(600)
+            .height(600)
+            .srcsetDescriptors(new ArrayList<String>() {{add("100w");}{add("200w");}{add("300w");} {add("400w");} {add("500w");} {add("600w");}})
             .alternateFormats(new ArrayList<String>() {{add("webp");}})
             .build();
 
@@ -71,7 +60,6 @@ public class CustomImageSizeProvider implements ImageSizeProvider {
         
         imageSizes.add(TALL);
         imageSizes.add(PORTRAIT);
-        imageSizes.add(SOCIAL_MEDIA);
         imageSizes.add(SQUARE);
         return imageSizes;
     }
